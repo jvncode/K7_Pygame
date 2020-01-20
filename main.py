e4ca4591cbcf233dc75ea919d689da1864687457
JVNCODE <jvn@MacBook-Pro-de-JESUS.local>
2020-01-20 23:57:54 +0100

    Animando al robot

--- main.py
+++ main.py
@@ -6,11 +6,31 @@ init()
 
 class Robot:
     speed = 5
-    images = ['robot_r01.png','robot_r02.png','robot_r03.png','robot_r04.png']
+    pictures = ['robot_r01.png','robot_r02.png','robot_r03.png','robot_r04.png']
+
+
     def __init__(self, x=0, y=0):
         self.x=x
         self.y=y
-        self.image = image.load('resources/robot_r01.png').convert_alpha()
+        self.frames = []
+        for pict in self.pictures:
+            frame = image.load('resources/{}'.format(pict)).convert_alpha()
+            self.frames.append(frame)
+
+        self.frame_act = 0
+        self.num_frames = len(self.frames)
+
+        # Otra opción de for:
+        # self.frames = [image.load('resources/{}'.format(pict)).convert_alpha() for pict in self.pictures]
+
+    def change_frame(self):
+        self.frame_act += 1
+        if self.frame_act == self.num_frames:
+            self.frame_act = 0
+
+        # Otra opción de código de este método:
+        #self.frame_act = (self.frame_act +1) % self.num_frames
+
 
     def go_up(self):
         '''
@@ -19,27 +39,41 @@ class Robot:
         '''
 
         self.y = max(0, self.y - self.speed)
+        self.change_frame()
     
     def go_down(self):
         self.y = min(600, self.y + self.speed)
+        self.change_frame()
 
     def go_left(self):
         self.x = max(0, self.x - self.speed)
+        self.change_frame()
 
     def go_right(self):
         self.x = min(800, self.x + self.speed)
+        self.change_frame()
+    
 
+    @property
     def position(self):
         return (self.x, self.y)
+    
+    @property
+    def image(self):
+        return self.frames[self.frame_act]
 
 
 screen = display.set_mode((800, 600))
 display.set_caption('Hola mundo')
+clock = time.Clock()
+
 background_color = (150, 150, 222)
 
 robot =  Robot(400, 300)
 
 while True:
+    clock.tick(60)
+
     for ev in event.get():
         if ev.type == QUIT:
             quit()
@@ -58,16 +92,20 @@ while True:
     keys_pressed = key.get_pressed()
     if keys_pressed[K_UP]:
         robot.go_up()
+    
     if keys_pressed[K_DOWN]:
         robot.go_down()
+
     if keys_pressed[K_RIGHT]:
         robot.go_right()
+
     if keys_pressed[K_LEFT]:
         robot.go_left()
 
 
+
     screen.fill(background_color)
-    screen.blit(robot.image, (robot.position()))
+    screen.blit(robot.image, robot.position)
 
     display.flip()
     
